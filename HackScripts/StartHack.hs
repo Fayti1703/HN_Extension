9c4f0a3195791dc8725a1af656465c7f8f0e2618
Fayti1703 <fayti1703@loop.de>
2017-06-09 19:14:04 +0200

    Work HackScript

---
+++ HackScripts/StartHack.hs
@@ -0,0 +1,41 @@
+config playerComp EmptyPC 0.2 $#%#$
+delay 2 $#%#$
+connect $#%#$
+delay 5 $#%#$
+openPort 22 $#%#$
+writel ____________________________$#%#$
+writel$#%#$
+writel WARNING - SSH PORT CRACKED  $#%#$
+writel ACCESS TO TERMINAL GRANTED  $#%#$
+writel ____________________________$#%#$
+delay 2$#%#$
+clearTerminal $#%#$
+delay 2 $#%#$
+writel Hey sorry for the scare.$#%#$
+delay 0.5 $#%#$
+writel I just noticed you didn't reply yet.$#%#$
+delay 0.5 $#%#$
+writel Everything okay here?$#%#$
+delay 5$#%#$
+writel $#%#$
+writel I'll just check, if you don't mind.$#%#$
+delay 10$#%#$
+writel $#%#$
+writel Ah... interesting.$#%#$
+writel I've never seen this x-server before, it's so bare bones....$#%#$
+delay 3$#%#$
+writel Hold on, I'll give you a proper one I picked up.$#%#$
+writel It's not the best, but, you'll learn to deal with it.$#%#$
+delay 5$#%#$
+writel$#%#$
+writel$#%#$
+writel$#%#$
+delay 1$#%#$
+writel Check this server:$#%#$
+writel It's pretty weak in terms of security, so break in and take it$#%#$
+writel IP: 356.576.888.259$#%#$
+writel$#%#$
+writel Remember to reboot once you got it.$#%#$
+writel $#%#$
+writel == [[ DISCONNECTED ]] ==$#%#$
+disconnect$#%#$
